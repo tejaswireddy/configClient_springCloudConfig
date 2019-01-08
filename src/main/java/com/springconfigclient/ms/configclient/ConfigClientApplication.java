e233599ac16d2c948fb3bc3a5304fa67c27f8580
tejaswireddy <tejaswireddy555@gmail.com>
2019-01-07 19:11:25 -0600

    Add files via upload

---
+++ src/main/java/com/springconfigclient/ms/configclient/ConfigClientApplication.java
@@ -0,0 +1,31 @@
+package com.springconfigclient.ms.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
+	}
+
+
+}
